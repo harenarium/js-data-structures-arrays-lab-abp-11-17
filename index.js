3369246c7c1b09b745e2efc79380fcb5c74c9596
harenarium <carolinel2610@gmail.com>
2017-11-09 18:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const app = "I don't do much."
+
+var driver = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  driver.push(name)
+  return driver
+}
+
+function destructivelyPrependKitten(name){
+  driver.unshift(name)
+  return driver
+}
+
+function destructivelyRemoveLastKitten(){
+  driver.pop()
+  return driver
+}
+
+function destructivelyRemoveFirstKitten(){
+  driver.shift()
+}
+
+function appendKitten(name){
+  return [...driver, name]
+}
+
+function prependKitten(name){
+  return [name, ...driver]
+}
+
+function removeLastKitten(){
+  return driver.slice(0,driver.length-1)
+}
+
+function removeFirstKitten(){
+  return driver.slice(1)
+}
